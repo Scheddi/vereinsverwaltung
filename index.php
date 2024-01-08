5815d1d509ae6f96c4db186b538fe0585f5c3763
Manuel Schedler <git@manuelschedler.de>
2024-01-08 19:54:52 +0100

    copy & cleanup

--- index.php
+++ index.php
@@ -3,80 +3,36 @@ session_start();
 $_SESSION['page']='home';
 $_SESSION['adminpage']=false;
 include "common/header.php";
-if(isset($_POST['proxy'])) {
-    $user = $_POST['proxy'];
-    $proxy = new User;
-    $proxy->load_by_id($user);
-}
-else {
-    $user = $_SESSION['userid'];
-}
-if(isset($_POST['insertAushilfe'])) {
-        $aushilfe = new Aushilfe;
-        $aushilfe->fill_from_array($_POST);
-        $aushilfe->save();
-}
-if(isset($_POST['deleteAushilfe'])) {
-        $aushilfe = new Aushilfe;
-        $aushilfe->load_by_id($_POST['Index']);
-        $aushilfe->delete();
-}
+$sql = sprintf('SELECT COUNT(`Index`) AS `Count` FROM `%sUser` WHERE `Deleted` != 1;',
+               $GLOBALS['dbprefix']
+);
+$dbr = mysqli_query($conn, $sql);
+sqlerror();
+$row = mysqli_fetch_array($dbr);
+$nMusiker = $row['Count'];
 ?>
-<script src="js/getStatus.js?<?php echo $GLOBALS['version']['Hash']; ?>"></script>
-<script src="js/melde.js?<?php echo $GLOBALS['version']['Hash']; ?>"></script>
-<script src="js/changeInstrument.js?<?php echo $GLOBALS['version']['Hash']; ?>"></script>
-<script src="js/meldeshift.js?<?php echo $GLOBALS['version']['Hash']; ?>"></script>
-<div class="w3-container <?php echo $GLOBALS['optionsDB']['colorTitleBar'] ;?>">
-    <h2>Bevorstehende Termine</h2>
-</div>
+    <div class="w3-container <?php echo $GLOBALS['optionsDB']['colorTitleBar']; ?>">
+          <h2>Liste aller Mitglieder (<?php echo $nMusiker; ?>)</h2>
+        </div>
+        
+        <div>
+        <input class="w3-input w3-border w3-padding" type="text" placeholder="Suchen..." id="filterString" onkeyup="filterLog()">
+        </div>
+        <div id="Liste">
 <?php
-$now = date("Y-m-d");
-if($GLOBALS['optionsDB']['entriesMainPage'] > 0) {
-    $sql = sprintf('SELECT `Index` FROM `%sTermine` WHERE `Datum` >= "%s" ORDER BY `Datum`, `Uhrzeit` LIMIT %s;',
-		   $GLOBALS['dbprefix'],
-		   $now,
-		   $GLOBALS['optionsDB']['entriesMainPage']
-    );
-}
-else {
-    $sql = sprintf('SELECT `Index` FROM `%sTermine` WHERE `Datum` >= "%s" ORDER BY `Datum`, `Uhrzeit`;',
-		   $GLOBALS['dbprefix'],
-		   $now,
-    );
-}
+        $sql = sprintf('SELECT `Index` FROM `%sUser` WHERE `Deleted` != 1 ORDER BY `Nachname`, `Vorname`;',
+                       $GLOBALS['dbprefix']
+        );
 $dbr = mysqli_query($conn, $sql);
 sqlerror();
 while($row = mysqli_fetch_array($dbr)) {
-    $M = new Termin;
+    $M = new User;
     $M->load_by_id($row['Index']);
-    $meldung = $M->getMeldungenByUser($user);
-    if($M->published > 0) {        
-        echo $M->printBasicTableLine();
-    }
-    elseif(requirePermission("perm_showHiddenAppmnts")) {
-        echo $M->printBasicTableLine();
-    }
-    elseif($meldung) {
-        echo $M->printBasicTableLine();        
-    }
+    $M->printUserTableLine();
 }
-if($GLOBALS['optionsDB']['showAppmntPage']) {
-    $more = new div;
-    $more->tag="a";
-    $more->class="w3-btn w3-hide-large w3-mobile w3-border w3-border-black w3-margin-bottom";
-    $more->class=$GLOBALS['optionsDB']['colorBtnSubmit'];
-    $more->href="termine.php";
-    $more->body="mehr Termine";
-    echo $more->print();
-    $moreL = new div;
-    $moreL->tag="a";
-    $moreL->class="w3-btn w3-hide-small w3-hide-medium w3-margin-left w3-margin-bottom w3-padding w3-border w3-border-black";
-    $moreL->class=$GLOBALS['optionsDB']['colorBtnSubmit'];
-    $moreL->href="termine.php";
-    $moreL->body="mehr Termine";
-    echo $moreL->print();
-}
-?>
+
+    ?>
+<script src="js/filterLog.js?<?php echo $GLOBALS['version']['Hash']; ?>"></script>
 <?php
 include "common/footer.php";
 ?>
